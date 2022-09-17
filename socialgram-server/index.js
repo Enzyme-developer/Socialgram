3232e9952b0dab25c3b2a5f39129a6d37438741b
Ayoe <dannyhopkins20003@gmail.com>
2022-09-17 09:58:40 +0100

    restructured server

---
+++ socialgram-server/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+dotenv.config()
+// import connectDb from './connectDb'
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+const PORT = 3000
+const start = async () => {
+    // try {
+    //     await connectDb(process.env.MONGO_URI)
+
+    // } catch (error) {
+    //     console.log(error);
+    // }
+    app.listen(PORT || 3000, () => console.log('server is listening')) 
+}
+
+start();
